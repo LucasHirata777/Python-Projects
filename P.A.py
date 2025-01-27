8cb6e909e4c10c7508905838bada6ffc8b3f2c64
Lucas Hirata <le.hirata@hotmail.com>
2025-01-27 15:05:18 -0300

    Create P.A.py
    Programa utiliza a linguagem Python para elaborar um Sistema de Progressão Aritmética.

---
+++ P.A.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10 - 1) * razão
+for c in range(primeiro,décimo2,razão):
+    print('{}'.format(c), end=' - ')
+print('Acabou!')
